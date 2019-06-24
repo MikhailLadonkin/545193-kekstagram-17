19c0952ade2dff88278027682e7c8b2696db1262
Mikhail <ladonkin.mikhail@gmail.com>
2019-06-24 09:50:57 +0300

    Added the modules

---
+++ js/pic-editor-window.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function () {
+  var uploadPicLabel = document.querySelector('#upload-file');
+  var uploadOverlay = document.querySelector('.img-upload__overlay');
+  var closeOverlay = document.querySelector('.img-upload__cancel');
+  var hashtagsField = document.querySelector('.text__hashtags');
+
+
+  var onPopupEscPress = function (evt) {
+    if (evt.keyCode === window.util.ESC_KEYCODE && document.activeElement !== window.commentField.element && document.activeElement !== hashtagsField) {
+      closePicEditor();
+    }
+  };
+
+  var openPicEditor = function () {
+    uploadOverlay.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closePicEditor = function () {
+    uploadOverlay.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
+
+  uploadPicLabel.addEventListener('change', function () {
+    openPicEditor();
+    window.changeOverlay();
+  });
+
+  closeOverlay.addEventListener('click', function () {
+    closePicEditor();
+  });
+})();
+
