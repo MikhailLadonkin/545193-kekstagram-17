10f2467b44618b999cc0d1069af3118658593536
Mikhail <ladonkin.mikhail@gmail.com>
2019-06-28 08:36:49 +0300

    Separated the functions properly

--- js/pictures.js
+++ js/pictures.js
@@ -1,26 +1,3 @@
-// 'use strict';
-
-// (function () {
-
-//   var template = document.querySelector('#picture').content.querySelector('a');
-//   var picturesDomElement = document.querySelector('.pictures');
-
-//   var renderPhotos = function (array) {
-//     var fragment = document.createDocumentFragment();
-//     for (var k = 0; k < array.length; k++) {
-//       var element = template.cloneNode(true);
-//       var item = array[k];
-//       element.querySelector('.picture__comments').textContent = item.comments.length;
-//       element.querySelector('.picture__likes').textContent = item.likes;
-//       element.querySelector('.picture__img').src = item.url;
-//       fragment.appendChild(element);
-//     }
-//     picturesDomElement.appendChild(fragment);
-//   };
-//   var photos = window.generateData(25);
-//   renderPhotos(photos);
-// })();
-
 'use strict';
 
 (function () {
@@ -28,7 +5,7 @@
   var template = document.querySelector('#picture').content.querySelector('a');
   var picturesDomElement = document.querySelector('.pictures');
 
-  var renderPhotos = function (item) {
+  var renderPhoto = function (item) {
     var element = template.cloneNode(true);
     element.querySelector('.picture__comments').textContent = item.comments.length;
     element.querySelector('.picture__likes').textContent = item.likes;
@@ -36,15 +13,18 @@
     return element;
   };
 
-
-  var successHandler = function (items) {
-
+  var renderPhotos = function (items) {
     var fragment = document.createDocumentFragment();
-    for (var k = 0; k < 25; k++) {
-      fragment.appendChild(renderPhotos(items[k]));
+    for (var k = 0; k < items.length; k++) {
+      fragment.appendChild(renderPhoto(items[k]));
     }
     picturesDomElement.appendChild(fragment);
   };
+
+  var successHandler = function () {
+    renderPhotos();
+  };
+
   var errorHandler = function (errorMessage) {
     var node = document.createElement('div');
     node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
@@ -56,8 +36,4 @@
     document.body.insertAdjacentElement('afterbegin', node);
   };
   window.load(successHandler, errorHandler);
-  // var photos = window.load(renderPhotos, errorHandler);
-  // renderPhotos(photos);
-  // console.log(renderPhotos(window.load))
-  // console.log(window.load)
 })();
