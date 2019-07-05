ab5fa32af903bf38a7c50f5d020e3e695c7b9b31
Mikhail <ladonkin.mikhail@gmail.com>
2019-07-05 17:24:50 +0300

    Updated the hashtags validation code

--- js/validation.js
+++ js/validation.js
@@ -15,52 +15,34 @@
   };
 
   var validateHashtags = function () {
-    var hashtagsList;
     var errorMessage;
+    var value = hashtagsInput.value.replace(/\s+/g, ' ').trim().toLowerCase();
+    var hashArr = value.split(' ');
 
-    if (hashtagsInput.value.indexOf(' ') <= -1) {
-      if (hashtagsInput.value.charAt(0) !== '#') {
-        errorMessage = 'The hashtag should start with "#"';
-      } else if (hashtagsInput.value.charAt(0) === '#' && hashtagsInput.value.length < 2) {
-        errorMessage = 'The hashtag should be informative';
-      } else if (hashtagsInput.value.length > 20) {
-        errorMessage = 'The hashtag can be up to 20 chars long';
-      } else if (hashtagsInput.value.charAt(0) === '#' && hashtagsInput.value.match(/#/g).length > 1) {
-        errorMessage = 'The hashtags should be separated';
-      } else {
-        hashtagsInput.style.border = '';
-        hashtagsInput.setCustomValidity('');
-      }
+    if (hashArr.length > 5) {
+      errorMessage = 'You can add up to 5 hashtags';
     }
 
-    if (hashtagsInput.value.indexOf(' ') > -1) {
-      hashtagsList = hashtagsInput.value.trim().split(' ');
-      if (hashtagsList.length > 5) {
-        errorMessage = 'You can add up to 5 hashtags';
+    for (var i = 0; i < hashArr.length; i++) {
+      var characters = hashArr[i];
+      var newHashtaagsList = hashArr.slice();
+      var repeatedHashtags = [];
+      for (var k = 0; k < newHashtaagsList.length - 1; k++) {
+        if (newHashtaagsList[k + 1].toLowerCase() === newHashtaagsList[k].toLowerCase()) {
+          repeatedHashtags.push(newHashtaagsList[i]);
+        }
       }
-      for (var i = 0; i < hashtagsList.length; i++) {
-        var characters = hashtagsList[i];
-        var newHashtaagsList = hashtagsList.slice();
-        var repeatedHashtags = [];
-
-        for (var k = 0; k < newHashtaagsList.length - 1; k++) {
-          if (newHashtaagsList[k + 1].toLowerCase() === newHashtaagsList[k].toLowerCase()) {
-            repeatedHashtags.push(newHashtaagsList[i]);
-          }
-        }
-
-        if (characters.charAt(0) !== '#') {
-          errorMessage = 'The hashtag should start with "#"';
-        } else if (characters.charAt(0) === '#' && characters.length < 2) {
-          var hashtagsElementNumber = characters.match(/#/g).length;
-          errorMessage = 'The hashtag should be informative';
-        } else if (characters.charAt(0) === '#' && hashtagsElementNumber > 1) {
-          errorMessage = 'The hashtags should be separated';
-        } else if (characters.length > 20) {
-          errorMessage = 'The hashtag can be up to 20 chars long';
-        } else if (repeatedHashtags.length > 0) {
-          errorMessage = 'The hashtags can not be repeated';
-        }
+      if (characters.charAt(0) !== '#') {
+        errorMessage = 'The hashtag should start with "#"';
+      } else if (characters.charAt(0) === '#' && characters.length < 2) {
+        var hashtagsElementNumber = characters.match(/#/g).length;
+        errorMessage = 'The hashtag should be informative';
+      } else if (characters.charAt(0) === '#' && hashtagsElementNumber > 1) {
+        errorMessage = 'The hashtags should be separated';
+      } else if (characters.length > 20) {
+        errorMessage = 'The hashtag can be up to 20 chars long';
+      } else if (repeatedHashtags.length > 0) {
+        errorMessage = 'The hashtags can not be repeated';
       }
     }
 
@@ -71,66 +53,6 @@
       hashtagsInput.style.border = '';
       hashtagsInput.setCustomValidity('');
     }
-    // if (hashtagsInput.value.indexOf(' ') <= -1) {
-    //   if (hashtagsInput.value.charAt(0) !== '#') {
-    //     hashtagsInput.style.border = 'thick solid red';
-    //     hashtagsInput.setCustomValidity('The hashtag should start with "#"');
-    //   } else if (hashtagsInput.value.charAt(0) === '#' && hashtagsInput.value < 2) {
-    //     hashtagsInput.style.border = 'thick solid red';
-    //     hashtagsInput.setCustomValidity('The hashtag should be informative');
-    //   } else if (hashtagsInput.value.charAt(0) === '#' && hashtagsInput.value.match(/#/g).length > 1) {
-    //     hashtagsInput.style.border = 'thick solid red';
-    //     hashtagsInput.setCustomValidity('The hashtags should be separated');
-    //   } else if (hashtagsInput.value.length > 20) {
-    //     hashtagsInput.style.border = 'thick solid red';
-    //     hashtagsInput.setCustomValidity('The hashtag can be up to 20 chars long');
-    //   } else {
-    //     hashtagsInput.style.border = '';
-    //     hashtagsInput.setCustomValidity('');
-    //   }
-    // }
-    // if (hashtagsInput.value.indexOf(' ') > -1) {
-    //   hashtagsList = hashtagsInput.value.trim().split(' ');
-    //   if (hashtagsList.length > 5) {
-    //     hashtagsInput.style.border = 'thick solid red';
-    //     hashtagsInput.setCustomValidity('You can add up to 5 hashtags');
-    //   } else {
-    //     hashtagsInput.style.border = '';
-    //     hashtagsInput.setCustomValidity('');
-    //     for (var i = 0; i < hashtagsList.length; i++) {
-    //       var characters = hashtagsList[i];
-    //       var hashtagsElementNumber = characters.match(/#/g).length;
-    //       var newHashtaagsList = hashtagsList.slice();
-    //       var repeatedHashtags = [];
-
-    //       for (var k = 0; k < newHashtaagsList.length - 1; k++) {
-    //         if (newHashtaagsList[k + 1].toLowerCase() === newHashtaagsList[k].toLowerCase()) {
-    //           repeatedHashtags.push(newHashtaagsList[i]);
-    //         }
-    //       }
-
-    //       if (characters.charAt(0) !== '#') {
-    //         hashtagsInput.style.border = 'thick solid red';
-    //         hashtagsInput.setCustomValidity('The hashtag should start with "#"');
-    //       } else if (characters.charAt(0) === '#' && characters.length < 2) {
-    //         hashtagsInput.style.border = 'thick solid red';
-    //         hashtagsInput.setCustomValidity('The hashtag should be informative');
-    //       } else if (characters.charAt(0) === '#' && hashtagsElementNumber > 1) {
-    //         hashtagsInput.style.border = 'thick solid red';
-    //         hashtagsInput.setCustomValidity('The hashtags should be separated');
-    //       } else if (characters.length > 20) {
-    //         hashtagsInput.style.border = 'thick solid red';
-    //         hashtagsInput.setCustomValidity('The hashtag can be up to 20 chars long');
-    //       } else if (repeatedHashtags.length > 0) {
-    //         hashtagsInput.style.border = 'thick solid red';
-    //         hashtagsInput.setCustomValidity('The hashtags can not be repeated');
-    //       } else {
-    //         hashtagsInput.style.border = '';
-    //         hashtagsInput.setCustomValidity('');
-    //       }
-    //     }
-    //   }
-    // }
   };
   commentField.addEventListener('change', validateCommentLength);
   hashtagsInput.addEventListener('change', validateHashtags);
